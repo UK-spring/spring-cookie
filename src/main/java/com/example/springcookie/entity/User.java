ead50913c3266b800367a164ba7a6209d89d05cc
UK <hwangwonuk94@gmail.com>
2024-10-29 15:16:42 +0900

    feat: User class 설계

---
+++ src/main/java/com/example/springcookie/entity/User.java
@@ -0,0 +1,14 @@
+package com.example.springcookie.entity;
+
+public class User {
+    // 식별자
+    private Long id;
+    // 이름
+    private String name;
+    // 나이
+    private Integer age;
+    // 로그인 ID
+    private String userName;
+    // 비밀번호
+    private String password;
+}
